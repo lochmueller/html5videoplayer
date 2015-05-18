21a657974ad9cd4aa4dc9b44edb65eeb9799ee0b
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-05-18 13:30:06 +0200

    Set dependencies to 6.2.0-7.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,29 +10,17 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-	'title' => 'HTML5 Video Player',
-	'description' => 'A video extension for TYPO3 built on the VideoJS HTML5 video player library. Allows you to embed video in your website using HTML5 with Flash fallback support for non-HTML5 browsers. Work on VideoJS 4.12.6 and support YouTube and Vimeo video in the same style.',
-	'category' => 'plugin',
-	'version' => '6.3.1',
-	'state' => 'stable',
-	'uploadfolder' => false,
-	'createDirs' => NULL,
-	'clearcacheonload' => false,
-	'author' => 'Tim Lochmueller',
+$EM_CONF[$_EXTKEY] = array(
+	'title'        => 'HTML5 Video Player',
+	'description'  => 'A video extension for TYPO3 built on the VideoJS HTML5 video player library. Allows you to embed video in your website using HTML5 with Flash fallback support for non-HTML5 browsers. Work on VideoJS 4.12.6 and support YouTube and Vimeo video in the same style.',
+	'category'     => 'plugin',
+	'version'      => '6.3.1',
+	'state'        => 'stable',
+	'author'       => 'Tim Lochmueller',
 	'author_email' => 'tim@fruit-lab.de',
-	'author_company' => NULL,
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '6.1.0-7.0.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
+	'constraints'  => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.2.99',
 		),
 	),
 );
