6e4736eb6f7b70c25daf5bc71abbaf4ba6254c8f
Tim Lochmüller <tim@fruit-lab.de>
2017-10-07 17:02:29 +0200

    Fix class name for Vimeo Hook

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,6 +11,6 @@ if (!defined('TYPO3_MODE')) {
 ]);
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['html5videoplayer_div'] = \HVP\Html5videoplayer\Div::class;
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['html5videoplayer_vimeo'] = \HVP\html5videoplayer\Hooks\VimeoProcessDatamap::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['html5videoplayer_vimeo'] = \HVP\Html5videoplayer\Hooks\VimeoProcessDatamap::class;
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['html5videoplayer_pivideoplayer']['html5videoplayer'] = \HVP\Html5videoplayer\Hooks\CmsLayout::class . '->getExtensionSummary';
