a23034b764df40a290bf5a4d34c278a30c23adc3
Ephraim Härer <ephraim.haerer@renolit.com>
2020-03-23 10:23:25 +0100

    updated PHP dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,14 +12,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'HTML5 Video Player',
     'description' => 'A video extension for TYPO3 built on the VideoJS HTML5 video player library. Allows you to embed video in your website using HTML5 with Flash fallback support for non-HTML5 browsers. Work on VideoJS 4.12.6 and support YouTube and Vimeo video in the same style.',
     'category' => 'plugin',
-    'version' => '9.5.0',
+    'version' => '9.5.1',
     'state' => 'stable',
     'author' => 'Tim Lochmueller',
     'author_email' => 'tim@fruit-lab.de',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
-            'php' => '7.0.0-7.3.99',
+            'php' => '7.1.0-7.3.99',
             'mediace' => '9.5.0-9.5.99'
         ],
     ],
